e97d642a95fe6e692ccff6033eb9f09586dd9fed
tobibanks <tobiodogwu@gmail.com>
2024-07-30 10:23:10 +0100

    umm

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -5,7 +5,6 @@ import { inter } from "../fonts/inter";
 import "../animations/animate.css";
 import AnimatedBody from "../animations/AnimatedBody";
 import AnimatedTitle from "../animations/AnimatedTitle";
-import AnimatedWords from "../animations/AnimatedWords";
 import { motion } from "framer-motion";
 import ContactBackground from "../components/background/ContactBackground";
 import React from "react";
